4620fec19f06d901124a19944e034be33a61dc96
Kota Amasaka <in9lude@gmail.com>
2023-05-03 22:34:30 +0900

    cameramanのplaywrightを使うコードをセットアップしました (#7)
    * chore: playwright-coreを依存に入れた
    
    * chore: ts-node-devを開発管環境の依存に追加
    
    * chore: ログを出した
    
    * feat: playwrightを使うコードをセットアップした
    
    * fix: URLをローカルのものに修正

--- packages/@nakayoshi/cameraman/src/main.ts
+++ packages/@nakayoshi/cameraman/src/main.ts
@@ -1,5 +1,34 @@
 import type { HttpFunction } from "@google-cloud/functions-framework";
+import { chromium } from "playwright-core";
 
-export const main: HttpFunction = async (_req, res) => {
-  res.send("Hello, world!");
+const THEATER_URL = process.env.THEATER_URL;
+if (!THEATER_URL) throw Error("THEATER_URLが読み込めません");
+
+export const main: HttpFunction = async (req, res) => {
+  const text = req.query.text;
+  if (typeof text !== "string") {
+    res.status(404).send("not found");
+    return;
+  }
+
+  const browser = await chromium.launch({ channel: "chrome", headless: true });
+  const page = await browser.newPage();
+
+  const url = new URL(THEATER_URL);
+  url.search = new URLSearchParams({
+    text,
+  }).toString();
+
+  await page.goto(url.toString());
+
+  const receivedTitle = await page.locator("h1").textContent();
+
+  await browser.close();
+
+  if (receivedTitle !== null) {
+    console.debug(`[ finished ] query: ${text}`);
+    res.send({ result: receivedTitle });
+    return;
+  }
+  res.send({ result: "" });
 };
