3e297f058b51170f5131fed6c29a851d1cd57c5c
sh1ma <in9lude@gmail.com>
2023-05-03 21:29:08 +0900

    chore: ログを出した

--- packages/@nakayoshi/cameraman/src/main.ts
+++ packages/@nakayoshi/cameraman/src/main.ts
@@ -1,5 +1,28 @@
 import type { HttpFunction } from "@google-cloud/functions-framework";
+import { chromium } from "playwright-core";
 
-export const main: HttpFunction = async (_req, res) => {
-  res.send("Hello, world!");
+const THEATER_URL = process.env.THEATER_URL;
+if (!THEATER_URL) throw Error("THEATER_URLが読み込めません");
+
+export const main: HttpFunction = async (req, res) => {
+  const text = req.query.text;
+  if (typeof text !== "string") {
+    await res.status(404).send("not found");
+    return;
+  }
+
+  const browser = await chromium.launch({ channel: "chrome", headless: true });
+  const page = await browser.newPage();
+  await page.goto(THEATER_URL + `?text=${text}`);
+
+  const receivedTitle = await page.locator("h1").textContent();
+
+  await browser.close();
+
+  if (receivedTitle !== null) {
+    console.debug(`[ finished ] query: ${text}`);
+    await res.send({ result: receivedTitle });
+    return;
+  }
+  await res.send({ result: "" });
 };
