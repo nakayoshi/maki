60341c7a5bdb2f57828f7a8d3be28568dfe7fa32
pinfort <ptg@nijitei.com>
2023-05-04 13:45:45 +0900

    rm component

--- packages/@nakayoshi/maki-cameraman/src/component/fileComponent.ts
+++
@@ -1,8 +0,0 @@
-interface FileComponent {
-  /**
-   * Upload file from local path
-   * @param path local path for file to upload
-   * @returns key of file uploaded
-   */
-  upload(path: string): Promise<string>;
-}
